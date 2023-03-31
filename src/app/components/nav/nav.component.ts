ba0be80da59e27aa0480901a835229201bb7d5d9
Afalkenhagen <ahau_buluk@protonmail.ch>
2023-03-31 19:46:24 -0300

    feat: functional hamburger navbar

--- src/app/components/nav/nav.component.ts
+++ src/app/components/nav/nav.component.ts
@@ -7,4 +7,22 @@ import { Component } from '@angular/core';
 })
 export class NavComponent {
 
+public active : boolean = false
+constructor(){
+
 }
+setActive():void{
+  this.active =!this.active
+  
+}
+
+}  
+
+
+// const hamburger = document.querySelector(".hamburger");
+// const navMenu = document.querySelector(".nav-menu");
+// hamburger?.addEventListener("click", mobileMenu);
+// function mobileMenu() {
+//       hamburger?.classList.toggle("active");
+//       navMenu?.classList.toggle("active");
+
