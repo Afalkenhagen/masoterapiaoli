b98563381bb6dd11aab429caf3e0dc60fd597757
Afalkenhagen <ahau_buluk@protonmail.ch>
2023-04-17 20:40:09 -0300

    feat: nav router basic setup

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { InicioComponent } from './pages/inicio/inicio.component';
+import { TerapiasComponent } from './pages/terapias/terapias.component';
+import { TurnosComponent } from './pages/turnos/turnos.component';
+import { ContactoComponent } from './pages/contacto/contacto.component';
+import { PagenotfoundComponent } from './pagenotfound/pagenotfound.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: "", component: InicioComponent},
+  {path: "terapias", component: TerapiasComponent},
+  {path: "turnos", component: TurnosComponent},
+  {path: "contacto", component: ContactoComponent},
+  {path: "**", pathMatch: 'full', component: PagenotfoundComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,13 +7,21 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NavComponent } from './components/nav/nav.component';
 import { ArticleComponent } from './components/article/article.component';
 import { FooterComponent } from './components/footer/footer.component';
+import { TerapiasComponent } from "./pages/terapias/terapias.component";
+import { InicioComponent } from './pages/inicio/inicio.component';
+import { TurnosComponent } from './pages/turnos/turnos.component';
+import { ContactoComponent } from './pages/contacto/contacto.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavComponent,
     ArticleComponent,
-    FooterComponent
+    FooterComponent,
+    TerapiasComponent,
+    InicioComponent,
+    TurnosComponent,
+    ContactoComponent
   ],
   imports: [
     BrowserModule,
